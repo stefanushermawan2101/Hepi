d4c2266fec7a0a2301b76e24992574d6b6b9a0d2
Stefanus Hermawan Sebastian <stefanushermawan2101@gmail.com>
2022-09-19 20:34:54 +0700

    update: finishing login UI

--- Hepi/Controller/Authentication/LoginController.swift
+++ Hepi/Controller/Authentication/LoginController.swift
@@ -44,6 +44,31 @@ class LoginController: UIViewController {
         return tf
     }()
     
+    private let loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Login", for: .normal)
+        button.setTitleColor(.brown, for: .normal)
+        button.backgroundColor = .white
+        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        button.layer.cornerRadius = 5
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        button.addTarget(self, action: #selector(handleLogin), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    private let dontHaveAccountButton: UIButton = {
+        let button = Utilities().attributedButton("Don't have an account? ", "Sign Up")
+        button.addTarget(self, action: #selector(handleShowSignUp), for: .touchUpInside)
+        return button
+    }()
+    
+    private let forgotPasswordButton: UIButton = {
+        let button = Utilities().attributedButton("", "Forgot Password?")
+        button.addTarget(self, action: #selector(handleShowForgotPassword), for: .touchUpInside)
+        return button
+    }()
+    
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
@@ -52,6 +77,17 @@ class LoginController: UIViewController {
     }
     
     // MARK: - Selectors
+    @objc func handleLogin() {
+        print("handle login")
+    }
+    
+    @objc func handleShowSignUp() {
+        print("masuk signup")
+    }
+    
+    @objc func handleShowForgotPassword() {
+        print("masuk forgot password")
+    }
     
     // MARK: - Helpers
     
@@ -65,9 +101,19 @@ class LoginController: UIViewController {
         
         let stack = UIStackView(arrangedSubviews: [emailContainerView, passwordContainerView])
         stack.axis = .vertical
-        stack.spacing = 8
+        stack.spacing = 20
+        stack.distribution = .fillEqually
         
         view.addSubview(stack)
         stack.anchor(top: logoImageView.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingLeft: 20, paddingRight: 20)
+        
+        view.addSubview(forgotPasswordButton)
+        forgotPasswordButton.anchor(top: stack.bottomAnchor, right: view.rightAnchor, paddingLeft: 20, paddingRight: 20)
+        
+        view.addSubview(loginButton)
+        loginButton.anchor(left: view.leftAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, right: view.rightAnchor, paddingLeft: 40, paddingBottom: 60, paddingRight: 40)
+        
+        view.addSubview(dontHaveAccountButton)
+        dontHaveAccountButton.anchor(left: view.leftAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, right: view.rightAnchor, paddingLeft: 40, paddingBottom: 20, paddingRight: 40)
     }
 }
--- Hepi/Utilities/Utilities.swift
+++ Hepi/Utilities/Utilities.swift
@@ -17,7 +17,7 @@ class Utilities {
         iv.image = image
         view.addSubview(iv)
         iv.anchor(left: view.leftAnchor, bottom: view.bottomAnchor, paddingBottom: 10)
-        iv.setDimensions(width: 25, height:22)
+        iv.setDimensions(width: 23.5, height:22)
         
         view.addSubview(textField)
         textField.anchor(left: iv.rightAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingBottom: 10)
@@ -37,5 +37,13 @@ class Utilities {
         tf.font = UIFont.systemFont(ofSize: 16)
         tf.attributedPlaceholder = NSAttributedString(string: placeholder, attributes: [NSAttributedString.Key.foregroundColor: UIColor.brown])
         return tf
+    }
+    
+    func attributedButton(_ firstPart: String, _ secondPart: String) -> UIButton {
+        let button = UIButton(type: .system)
+        let attributedTitle = NSMutableAttributedString(string: firstPart, attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 16), NSAttributedString.Key.foregroundColor: UIColor.brown])
+        attributedTitle.append(NSAttributedString(string: secondPart, attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 16), NSAttributedString.Key.foregroundColor: UIColor.brown]))
+        button.setAttributedTitle(attributedTitle, for: .normal)
+        return button
     }
 }
